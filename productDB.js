09a7812f1ddf101a0ae8e905abbb42c7444ffa7c
Nikhil-Carpenter <nikhilcrpntr222@gmail.com>
2023-04-01 14:43:35 +0530

    update

--- productDB.js
+++ productDB.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose")
 const connectDB = require("./db/connect");
+const url = process.env.MONGODB_URL || 5000;
 
 const Product  = require("./models/products")
 const productJson = require("./productData.json")
@@ -8,7 +9,7 @@ const productJson = require("./productData.json")
 mongoose.set('strictQuery', false)
 const start = async()=>{
     try {
-        await connectDB("mongodb+srv://nikhilcarpenter:Mongodbatlas07pass@cluster0.zo6wkso.mongodb.net/backApi?retryWrites=true&w=majority")
+        await connectDB(url)
         await Product.create(productJson)
         console.log("Success");
     } catch (error) {
