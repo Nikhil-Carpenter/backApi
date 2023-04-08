8b67efed901188f08c6dcdb34761551af639f799
Nikhil-Carpenter <nikhilcrpntr222@gmail.com>
2023-04-08 13:59:21 +0530

    delete working

--- controllers/products.js
+++ controllers/products.js
@@ -1,45 +1,67 @@
 // const ApiData = require("../productData.json");
 const Product = require("../models/products");
 
-const getAllProducts =  async (req, resp) => {
-  const mydata = await Product.find()
-    resp.status(200).send(mydata);
-    // console.log(ApiData);
-  }
+const getAllProducts = async (req, resp) => {
+  const mydata = await Product.find();
+  resp.status(200).send(mydata);
+  // console.log(ApiData);
+};
 
 const getOneProduct = async (req, resp) => {
   const id = req.params.id;
-  const product = await Product.findOne({_id:id});
+  const product = await Product.findOne({ _id: id });
   // console.log(id);
   // console.log(product);
-  return resp.json(product);
-}
+  if (product) {
+    return resp.json(product);
+  }else{
+    return resp.json("Product not Exist")
+  }
+};
 
-const createProduct = async (req,resp)=>{
+const createProduct = async (req, resp) => {
   try {
     let body = req.body;
     // console.log(body);
-    let product = await Product.create(body)
-    resp.send(product)
+    let product = await Product.create(body);
+    resp.send(product);
   } catch (error) {
     console.log(error);
   }
-}
+};
 
-  const updateProduct = async (req, res) => {
-   try {
+const updateProduct = async (req, res) => {
+  try {
     let id = req.params.id;
     let data = req.body;
     // console.log(id);
     // console.log(data)
-    const product = await Product.updateOne({_id:id}, data);
-    const updatedProduct = await Product.findById(id)
-    res.send({success:true,message:"Product Updated",product:updatedProduct})
-
-   } catch (error) {
+    const product = await Product.updateOne({ _id: id }, data);
+    const updatedProduct = await Product.findById(id);
+    res.send({
+      success: true,
+      message: "Product Updated",
+      product: updatedProduct,
+    });
+  } catch (error) {
     console.log(error);
-   }
-    
-}
+  }
+};
 
-module.exports = {getAllProducts,updateProduct,getOneProduct,createProduct};
+const deleteProduct = async (req, resp) => {
+  const id = req.params.id;
+  // console.log(id);
+  const product = await Product.deleteOne({ _id: id });
+  resp.send({
+    success: true,
+    message: "Product Deleted",
+  });
+};
+
+module.exports = {
+  getAllProducts,
+  updateProduct,
+  getOneProduct,
+  createProduct,
+  deleteProduct,
+};
--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const {getAllProducts,updateProduct,getOneProduct ,createProduct} = require("../controllers/products")
+const {
+  getAllProducts,
+  updateProduct,
+  getOneProduct,
+  createProduct,
+  deleteProduct,
+} = require("../controllers/products");
 
-router.route('/').get(getAllProducts)
-router.route('/').post(createProduct)
-router.route('/:id').put(updateProduct)
+router.route("/").get(getAllProducts);
+router.route("/").post(createProduct);
+router.route("/:id").put(updateProduct);
+router.route("/:id").delete(deleteProduct);
 
-router.route('/:id').get(getOneProduct)
-
+router.route("/:id").get(getOneProduct);
 
 module.exports = router;
